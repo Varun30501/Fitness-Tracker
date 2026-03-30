61b04e58de68e78bb88da511c4ef69279be15ab4
Varun30501 <vatsanvenky@gmail.com>
2026-03-30 16:01:19 +0530

    second commit

--- Client/src/configs/api.ts
+++ Client/src/configs/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_STRAPI_API_URL || "http://localhost:1337",
+    baseURL: import.meta.env.VITE_STRAPI_API_URL || "https://timely-passion-8556e12d20.strapiapp.com",
 })
 
 export default api;
